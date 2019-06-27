07ae30a2475879593d8088da8706921ffbb48e20
nataliereshetnikova <ab1748@gmail.com>
2019-06-27 15:52:21 -0400

    Updates

---
+++ precache-manifest.89ae282086db4c60bd51f1da13193d98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bc697e485b2c6908c283b289949807",
+    "url": "/random-boxes/index.html"
+  },
+  {
+    "revision": "2ea4021cf1484c04da96",
+    "url": "/random-boxes/static/css/main.fe1f4f35.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/random-boxes/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "2ea4021cf1484c04da96",
+    "url": "/random-boxes/static/js/main.2cee7f11.chunk.js"
+  },
+  {
+    "revision": "59038e08ce4ec7450c72",
+    "url": "/random-boxes/static/js/runtime~main.5d001ed0.js"
+  }
+]);
